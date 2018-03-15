10614129f1f455a42ebfaf0590ed0514800312d1
Alex Dulub <alex.dulub@designsters.com>
2018-03-15 17:57:52 +0300

    Changed assert => require.

--- kyc.sol
+++ kyc.sol
@@ -13,32 +13,32 @@ contract KYC is Admins {
     mapping(address => UserInfo) public informations;
 
     function createUserInfo(address user, bytes32 hashData, uint valueNodes) external onlyServer {
-        assert(informations[user].hashData == 0 && hashData != 0);
-        assert(informations[user].valueNodes == 0 && valueNodes > 0);
-        assert(informations[user].valid == false);
+        require(informations[user].hashData == 0 && hashData != 0);
+        require(informations[user].valueNodes == 0 && valueNodes > 0);
+        require(informations[user].valid == false);
 
         informations[user] = UserInfo(hashData, valueNodes, false);
     }
 
     function changeUserInfo(address user, bytes32 hashData, uint valueNodes) external onlyServer {
-        assert(informations[user].hashData != 0);
-        assert(informations[user].valueNodes > 0 );
+        require(informations[user].hashData != 0);
+        require(informations[user].valueNodes > 0 );
 
         informations[user] = UserInfo(hashData, valueNodes, false);
     }
 
     function userAuthorization() external payable {
-        assert(informations[msg.sender].hashData != 0);
-        assert(informations[msg.sender].valueNodes > 0 );
-        assert(informations[msg.sender].valid == false);
-        assert(informations[msg.sender].valueNodes < (msg.value * 100));
+        require(informations[msg.sender].hashData != 0);
+        require(informations[msg.sender].valueNodes > 0 );
+        require(informations[msg.sender].valid == false);
+        require(informations[msg.sender].valueNodes < (msg.value * 100));
 
         informations[msg.sender].valid = true;
     }
 
     function isAuthorized(address user) external constant returns(bool) {
-        assert(informations[user].hashData != 0);
-        assert(informations[user].valueNodes > 0 );
+        require(informations[user].hashData != 0);
+        require(informations[user].valueNodes > 0 );
 
         return informations[user].valid;
     }
